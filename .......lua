8f6b38a5a8a7eaf09e7b369b8cf9547db9f97bf2
Tgoduiem <vn.toocn@gmail.com>
2025-01-01 23:33:19 +0700

    Update .......lua

--- .......lua
+++ .......lua
@@ -26,3 +26,51 @@ local Tabs = {
     Misc = Window:AddTab({ Title = "Misc", Icon = "" })
 }
 local Options = Fluent.Options
+-- Windows
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
+local MainScreenGui = Instance.new("ScreenGui")
+local ButtonScreenGui = Instance.new("ScreenGui")
+local MainFrame = Instance.new("Frame")
+local ImageButton = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
+MainScreenGui.Name = "MainScreenGui"
+MainScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+MainScreenGui.Enabled = false
+
+MainFrame.Name = "MainFrame"
+MainFrame.Parent = MainScreenGui
+MainFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+MainFrame.BackgroundTransparency = 1
+MainFrame.Size = UDim2.new(1, 0, 1, 0)
+MainFrame.Draggable = true
+
+ButtonScreenGui.Name = "ButtonScreenGui"
+ButtonScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+ImageButton.Parent = ButtonScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(255,250,147)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.1, 0, 0, 10)
+ImageButton.AnchorPoint = Vector2.new(0, 0)
+ImageButton.Size = UDim2.new(0, 25, 0, 25)
+ImageButton.Image = "rbxassetid://91853848472964"
+ImageButton.Draggable = true
+
+UICorner.CornerRadius = UDim.new(0.5, 0)
+UICorner.Parent = ImageButton
+
+ImageButton.MouseButton1Click:Connect(function()
+    game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.LeftControl, false, game)
+
+    local scaleUpTween = TweenService:Create(ImageButton, TweenInfo.new(0.1), { Size = UDim2.new(0, 45, 0, 45) })
+    local scaleDownTween = TweenService:Create(ImageButton, TweenInfo.new(0.1), { Size = UDim2.new(0, 35, 0, 30) })
+
+    scaleUpTween:Play()
+    scaleUpTween.Completed:Connect(function()
+        scaleDownTween:Play()
+    end)
+end)
