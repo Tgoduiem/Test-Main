076e51e063bf091faf31a2495fa5ec8804bb0a28
Tgoduiem <vn.toocn@gmail.com>
2025-01-02 19:03:55 +0700

    Add files via upload

---
+++ ...lua
@@ -0,0 +1,78 @@
+if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
+local plr = game.Players.LocalPlayer
+
+local Players = game:GetService("Players")
+local Title = "....."
+local SubTitle = "By Ztx" --Main Rewrite discord.gg/9PzEWrzgXR
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenLam2504/rblx/refs/heads/main/gay.lua"))()
+local Window = getgenv().Window or Fluent:CreateWindow({
+    Title = Title,
+    SubTitle = SubTitle,
+    TabWidth = 160,
+    Size = UDim2.fromOffset(480, 360),
+    Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.End
+})
+local Tabs = {
+    Settings = Window:AddTab({ Title = "Setting", Icon = "" }),
+    Status = Window:AddTab({ Title = "Status",Icon = "" }),
+    NguoiChoi = Window:AddTab({ Title = "Local Player", Icon = "" }),
+    Shop = Window:AddTab({ Title = "Shop", Icon = "" }),
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    SubFram = Window:AddTab({ Title = "Sub Farming", Icon = "" }),
+    OtherFram = Window:AddTab({ Title = "Other Framing", Icon = "" }),
+    Item = Window:AddTab({ Title = "Get Item", Icon = "" }),
+    Race = Window:AddTab({ Title = "RaceV4-Mirage", Icon = "" }),
+    Event = Window:AddTab({ Title = "Sea Event", Icon = "" }),
+    Raid = Window:AddTab({ Title = "Raid - Material", Icon = "" }),
+    Pvp = Window:AddTab({ Title = "PVP-Aimbot", Icon = "" })
+}
+
+local Options = Fluent.Options
+-- Windows
+
+
+SelectWeapon = Tabs.Settings:AddDropdown("SelectWeapon", {
+        Title = "Select Weapon",
+        Values = { "Melee", "Sword" },
+        Multi = false,
+        Default = 1,
+    })
+
+    SelectWeapon:SetValue("Melee")
+
+    SelectWeapon:OnChanged(function(Value)
+        _G.SelectWeapon = Value
+    end)
+
+    task.spawn(function()
+        while wait() do
+            pcall(function()
+                if _G.SelectWeapon == "Melee" then
+                    for _, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                        if v.ToolTip == "Melee" then
+                            if game.Players.LocalPlayer.Backpack:FindFirstChild(tostring(v.Name)) then
+                                _G.SelectWeapon = v.Name
+                            end
+                        end
+                    end
+                elseif _G.SelectWeapon == "Sword" then
+                    for _, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                        if v.ToolTip == "Sword" then
+                            if game.Players.LocalPlayer.Backpack:FindFirstChild(tostring(v.Name)) then
+                                _G.SelectWeapon = v.Name
+                            end
+                        end
+                    end
+                end
+            end)
+        end
+    end)
+Toggle = Tabs.Settings:AddToggle("MyToggle", {Title = "Auto Haki", Default = true })
+    Toggle:OnChanged(function(Value)
+        _G.AUTOHAKI = Value
+    end)
+  end
+end
+
