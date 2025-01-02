3e96ccd6de376e1a678486894cc53f8578657884
Tgoduiem <vn.toocn@gmail.com>
2025-01-02 18:45:07 +0700

    Update .......lua

--- .......lua
+++ .......lua
@@ -86,17 +86,4 @@ SelectWeapon = Tabs.Setting:AddDropdown("SelectWeapon", {
     end)
     Toggle = Tabs.Setting:AddToggle("Toggle", {Title = "Auto Haki", Default = true })
     Toggle:OnChanged(function(Value)
-        _G.AUTOHAKI = Value
     end)
-    spawn(function()
-    while wait(.1) do
-        if _G.AUTOHAKI then 
-            if not game.Players.LocalPlayer.Character:FindFirstChild("HasBuso") then
-                local args = {
-                    [1] = "Buso"
-                }
-                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-            end
-        end
-    end
-end)
