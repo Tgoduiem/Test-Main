b967a8c4d87d198e8890de931586a17d5b860fde
Tgoduiem <vn.toocn@gmail.com>
2025-01-02 13:02:05 +0700

    Update .......lua

--- .......lua
+++ .......lua
@@ -31,40 +31,41 @@ local Tabs = {
 
 local Options = Fluent.Options
 -- Windows
-local player = game.Players.LocalPlayer
-local L1 = Instance.new("ScreenGui")
-local L2 = Instance.new("TextButton")
-local L3 = Instance.new("UICorner")
-local L4 = Instance.new("ImageLabel")
-local sound = Instance.new("Sound")
-L3.Name = "UICorner"
-L3.Parent = L2
-L4.Name = "ButtonImage"
-L4.Parent = L2
-L4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-L4.BackgroundTransparency = 1.000
-L4.BorderSizePixel = 0
-L4.Position = UDim2.new(0.1, 0, 0.1, 0) 
-L4.Size = UDim2.new(0, 45, 0, 45)
-L4.Image = ""
-L1.Name = "MainGui"
-L1.Parent = player:WaitForChild("PlayerGui")
-L1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-L2.Name = "CustomButton"
-L2.Parent = L1
-L2.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-L2.BackgroundTransparency = 0.2
-L2.BorderSizePixel = 0
-L2.Position = UDim2.new(0.1208, 0, 0.0953, 0)
-L2.Size = UDim2.new(0, 50, 0, 50)
-L2.Font = Enum.Font.LuckiestGuy
-L2.Text = "HadesGod\n Premium"
-L2.TextColor3 = Color3.fromRGB(255, 255, 255)
-L2.TextSize = 9.000
-L2.Draggable = true
-sound.Parent = L2
-sound.SoundId = "rbxassetid://130785805"
-L2.MouseButton1Click:Connect(function()
-    game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.LeftControl, false, game)
-    sound:Play()
-end)
+
+
+SelectWeapon = Tabs.Setting:AddDropdown("SelectWeapon", {
+        Title = "Select Weapon",
+        Values = { "Melee", "Sword" },
+        Multi = false,
+        Default = 1,
+    })
+
+    SelectWeapon:SetValue("Melee")
+
+    SelectWeapon:OnChanged(function(Value)
+        _G.SelectWeapon = Value
+    end)
+
+    task.spawn(function()
+        while wait() do
+            pcall(function()
+                if _G.SelectWeapon == "Melee" then
+                    for _, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                        if v.ToolTip == "Melee" then
+                            if game.Players.LocalPlayer.Backpack:FindFirstChild(tostring(v.Name)) then
+                                _G.SelectWeapon = v.Name
+                            end
+                        end
+                    end
+                elseif _G.SelectWeapon == "Sword" then
+                    for _, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                        if v.ToolTip == "Sword" then
+                            if game.Players.LocalPlayer.Backpack:FindFirstChild(tostring(v.Name)) then
+                                _G.SelectWeapon = v.Name
+                            end
+                        end
+                    end
+                end
+            end)
+        end
+    end)
